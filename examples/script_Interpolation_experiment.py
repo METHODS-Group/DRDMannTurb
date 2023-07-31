d5aaa9a04958d46cef6763734759ac1c1b5c1083
mjachi <mmmeeeker@gmail.com>
2023-07-30 23:25:54 -0400

    Interpolation experiment scaled incorrectly but works end to end

--- examples/script_Interpolation_experiment.py
+++ examples/script_Interpolation_experiment.py
@@ -6,6 +6,7 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
+import torch.nn as nn
 from fracturbulence.Calibration import CalibrationProblem
 from fracturbulence.common import MannEddyLifetime
 from fracturbulence.DataGenerator import OnePointSpectraDataGenerator
@@ -14,57 +15,61 @@ from typing import Optional
 
 plt.style.use("bmh")
 
-"""
-The below segment defines all constants required below
-"""
+plt.rc("text", usetex=True)
+plt.rc("font", family="serif")
 
 if torch.cuda.is_available():
     torch.set_default_tensor_type("torch.cuda.FloatTensor")
 
+"""
+Set constants, etc.
+"""
+
 savedir = Path(__file__).parent / "results"
 
 CONSTANTS_CONFIG = {
-    "type_EddyLifetime": "customMLP",  # CALIBRATION : 'tauNet',  ### 'const', TwoThird', 'Mann', 'customMLP', 'tauNet'
-    "type_PowerSpectra": "RDT",  # 'RDT', 'zetaNet', 'C3Net', 'Corrector'
-    "learn_nu": False,  # NOTE: Experiment 1: False, Experiment 2: True
+    "type_EddyLifetime": "customMLP",
+    "type_PowerSpectra": "RDT",
+    "learn_nu": False,
     "plt_tau": True,
-    "hlayers": [10, 10],  # ONLY NEEDED FOR CUSTOMNET OR RESNET
-    "tol": 1.0e-9,  # not important
-    "lr": 1,  # learning rate
-    "penalty": 1,  # CALIBRATION: 1.e-1,
-    "regularization": 1.0e-5,  # CALIBRATION: 1.e-1,
-    "nepochs": 10,
+    "hlayers": [10, 10],
+    "tol": 1.0e-9,
+    "lr": 1,
+    "penalty": 1,
+    "regularization": 1.0e-5,
+    "nepochs": 5,
     "curves": [0, 1, 2, 3],
-    "data_type": "Custom",  # CALIBRATION: 'Custom', ### 'Kaimal', 'SimiuScanlan', 'SimiuYeo', 'iso'
-    # "data_type": "Auto",  # CALIBRATION: 'Custom', ### 'Kaimal', 'SimiuScanlan', 'SimiuYeo', 'iso'
+    "data_type": "Kaimal",
+    # "spectra_file": "constants/Spectra.dat",
     "spectra_file": "Spectra_exp.dat",
-    "Uref": 10,  # m/s
-    "zref": 1,  # m
-    "domain": None,
-    "noisy_data": 0.0,  # 0*3.e-1, ### level of the data noise  ### NOTE: Experiment 1: zero, Experiment 2: non-zero
+    # "Uref": 10,
+    "Uref": 21,
+    "zref": 1,
+    "domain": torch.logspace(-1, 2, 20),  # NOTE: This gets updated in the script portion
+    "noisy_data": 0.0,
+    "output_folder": str(savedir),
 }
 
 zref = CONSTANTS_CONFIG["zref"]
-# Hub height in meters
 Uref = CONSTANTS_CONFIG["Uref"]
-# Average Hub height velocity in m/s
 Iref = 0.14
 sigma1 = Iref * (0.75 * Uref + 5.6)
 Lambda1 = 42
-# Longitudinal turbulence scale parameter at hub height
 
 z0 = 0.01
 ustar = 0.41 * Uref / np.log(zref / z0)
 
-# NOTE: values taken from experiment1 in the paper
-L = 0.59
-GAMMA = 3.9
-SIGMA = 3.2
+# L = 0.59
+# GAMMA = 3.9
+# SIGMA = 3.2
 
+L = 70
+GAMMA = 3.7
+SIGMA = 3.2
+# UREF = 21
 
 """
-The below segment plots the spectra data interpolated over a
-common set of x coordinates
+Define the File IO routines
 """
 
 
@@ -90,7 +95,8 @@ def extract_x_spectra(filepath: Path) -> tuple[np.ndarray, np.ndarray]:
             x.append(float(row[0]))
             spectra.append(float(row[1]))
 
-    return np.log10(np.array(x)), np.array(spectra)
+    # return np.log10(np.array(x)), np.array(spectra)
+    return np.array(x), np.array(spectra)
 
 
 def export_into_spectra(
@@ -124,7 +130,7 @@ def export_into_spectra(
 
     filename = filename + ".dat"
 
-    with open(filename, 'w') as csvfile:
+    with open(filename, "w") as csvfile:
         csvwriter = csv.writer(csvfile)
         csvwriter.writerows(contents)
 
@@ -155,7 +161,25 @@ def interp_spectra(
     return cs(x_interp)
 
 
-def driver_interp(plot_interp: bool):
+"""
+Given the following data files, the following driving function will return
+"""
+
+
+def interpolate(plot_interp: bool) -> tuple[np.ndarray, ...]:
+    """
+    Calculates and returns the interpolations over a common set of x-coordinates
+
+    Parameters
+    ----------
+    plot_interp : bool
+        If True, will plot the interpolations after. Else just returns.
+
+    Returns
+    -------
+    tuple[np.ndarray, ...]
+        Tuple of x_interp, interp_u, interp_v, interp_w, interp_uw
+    """
     x_coords_u, u_spectra = extract_x_spectra(
         Path().resolve() / "data" / "u_spectra.csv"
     )
@@ -181,12 +205,10 @@ def driver_interp(plot_interp: bool):
     # The below will plot the data along with the cubic spline interpolation
     if plot_interp:
         print("Plotting interpolations of experimental data")
-        # cs = CubicSpline(x_coords_w, w_spectra)
-        # y_interp = cs(x_interp)
-        # y_cs_true = cs(x_coords_u)
-        # cmap = mpl.cm.get_cmap('Spectral')
+
         cmap = plt.get_cmap("Spectral", 4)
         custom_palette = [mpl.colors.rgb2hex(cmap(i)) for i in range(cmap.N)]
+
         plt.plot(
             x_coords_u,
             u_spectra,
@@ -195,6 +217,7 @@ def driver_interp(plot_interp: bool):
             color=custom_palette[0],
         )
         plt.plot(x_interp, interp_u, color=custom_palette[0])
+
         plt.plot(
             x_coords_v,
             v_spectra,
@@ -203,6 +226,7 @@ def driver_interp(plot_interp: bool):
             color=custom_palette[1],
         )
         plt.plot(x_interp, interp_v, color=custom_palette[1])
+
         plt.plot(
             x_coords_w,
             w_spectra,
@@ -211,6 +235,7 @@ def driver_interp(plot_interp: bool):
             color=custom_palette[2],
         )
         plt.plot(x_interp, interp_w, color=custom_palette[2])
+
         plt.plot(
             x_coords_uw,
             uw_cospectra,
@@ -220,69 +245,64 @@ def driver_interp(plot_interp: bool):
         )
         plt.plot(x_interp, interp_uw, color=custom_palette[3])
 
-        # mse_interp = mean_squared_error(y_cs_true, w_spectra)
-        # plt.yscale('log')
-        # plt.xscale('log')
-        # plt.title(f"MSE of Interpolant = {mse_interp}")
         plt.title("Spectra Interpolation")
         plt.legend()
         plt.show()
 
-    return np.power(10, x_interp), interp_u, interp_v, interp_w, interp_uw
+    return x_interp, interp_u, interp_v, interp_w, interp_uw
 
 
-def driver(x_interp, plot_result=False):
+"""
+Define the driving functions
+"""
+
+
+def driver(plot_result: bool) -> None:
     """
     Driving function
-    """
 
-    # Set up config
-    CONSTANTS_CONFIG["domain"] = torch.from_numpy(x_interp)
+    Parameters
+    ----------
+    plot_result : bool
+        If true, plots results
+    """    
+    config = CONSTANTS_CONFIG
+    config["activations"] = [nn.ReLU(), nn.ReLU(), nn.ReLU()]
+    config["hlayers"] = [32] * 4
 
-    # Create calibration problem
-    pb = CalibrationProblem(**CONSTANTS_CONFIG)
-
-    # Set up pb.parameters
+    pb = CalibrationProblem(**config)
     parameters = pb.parameters
-    parameters[:3] = [
-        np.log(L),
-        np.log(GAMMA),
-        np.log(SIGMA),
-    ]  # All of these parameters are positive
-    # so we can train the NN for the log of these parameters.
-    pb.parameters = parameters[:len(pb.parameters)]
+    parameters[:3] = [np.log(L), np.log(GAMMA), np.log(SIGMA)]
 
-    k1_data_pts = CONSTANTS_CONFIG["domain"]
-    spectra_file = CONSTANTS_CONFIG['spectra_file']
-    print("READING FILE " + spectra_file + "\n")
-    CustomData = torch.from_numpy(np.genfromtxt(spectra_file, delimiter=","))
-    print(3 * "\n")
-    print(CustomData)
-    print(3 * "\n")   
-    input()
-
-    f = CustomData[:, 0]
-
-
+    pb.parameters = parameters[: len(pb.parameters)]
+    k1_data_pts = config["domain"]
     DataPoints = [(k1, 1) for k1 in k1_data_pts]
-    Data = OnePointSpectraDataGenerator(DataPoints=DataPoints, **CONSTANTS_CONFIG).Data
+    Data = OnePointSpectraDataGenerator(DataPoints=DataPoints, **config).Data
 
     DataValues = Data[1]
 
     IECtau = MannEddyLifetime(k1_data_pts * L)
     kF = pb.eval(k1_data_pts)
 
-    opt_params = pb.calibrate(Data=(Data, DataValues), **CONSTANTS_CONFIG)
+    opt_params = pb.calibrate(Data=Data, **config)
 
     if plot_result:
+        plt.figure()
+
         plt.plot(pb.loss_history_epochs, "o-", label="Epochs Loss History")
         plt.legend()
         plt.xlabel("Epoch Number")
         plt.ylabel("MSE")
         plt.yscale("log")
 
+        plt.show()
+
 
 if __name__ == "__main__":
+    """
+    Script segment
+    """
+
     parser = argparse.ArgumentParser(
         description="experimenting with the interpolations",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
@@ -302,9 +322,14 @@ if __name__ == "__main__":
     )
 
     args = parser.parse_args()
+    if args.plot_interp:
+        print("Will plot interp")
+    if args.plot_result:
+        print("Will plot result")
 
-    print("Interpolating")
-    out_tuple = driver_interp(args.plot_interp)
-    export_into_spectra(*out_tuple)
-    print("Running through the RDT NN")
-    driver(out_tuple[0], plot_result=args.plot_result)
+    x_interp, interp_u, interp_v, interp_w, interp_uw = interpolate(args.plot_interp)
+    
+    # NOTE: update the config to the problem from above
+    CONSTANTS_CONFIG['domain'] = torch.from_numpy(x_interp)
+
+    driver(args.plot_result)
