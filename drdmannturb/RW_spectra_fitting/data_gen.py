daca6b7c89b2e1fe7f492a5561eb65159fb09fe2
Matthew Meeker <meeker.matthew@pm.me>
2025-05-07 13:00:16 -0400

    Docstring edits

---
+++ drdmannturb/RW_spectra_fitting/data_gen.py
@@ -0,0 +1,9 @@
+"""Synthetic data generators and some pre-processing utilities."""
+
+import numpy as np
+import torch
+
+
+def generate_kaimal_data(k1_vals: torch.Tensor, zref: float, ustar: float) -> torch.Tensor:
+    r"""Generate synthetic data using the Kaimal spectra."""
+    pass
