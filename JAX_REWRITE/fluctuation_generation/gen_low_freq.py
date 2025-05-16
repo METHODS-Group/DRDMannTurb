cec4cbea5a2ffdd4139ff87c974ad1bcea812688
Matthew Meeker <meeker.matthew@pm.me>
2025-05-16 15:20:29 -0400

    Significant clean up

--- JAX_REWRITE/fluctuation_generation/gen_low_freq.py
+++ JAX_REWRITE/fluctuation_generation/gen_low_freq.py
@@ -18,43 +18,4 @@ from scipy import integrate
 LowFreqConfig = Dict[str, float]
 
 
-def generate_low_frequency_field(
-    sigma2: float,
-    L_2d: float,
-    psi: float,
-    z_i: float,
-) -> Float[Array, "N1 N2 2"]:  # noqa: F722
-    """Generate a low-frequency fluctuation field.
 
-    This function is not intended for direct use. Instead, use the `FluctuationGenerator` class.
-
-    This implements a low-frequency turbulence model calibrated against marine environments and is
-    the 2D component of a so-called 2D+3D turbulence model, which is obtained by generating a 3D
-    field and, separately, this 2D field and then adding the 2D field to each vertical level of the 3D field.
-
-    Parameters
-    ----------
-    sigma2 : float
-        Turbulence intensity.
-    L_2d : float
-        Integral length scale.
-    psi : float
-        Direction of the mean wind.
-    Lx : float
-        Length of the domain in the x-direction.
-    Ly : float
-        Length of the domain in the y-direction.
-    Nx : int
-        Grid levels in the x-direction.
-
-        This means that there will be (2**Nx + 1) grid points in the x-direction.
-    Ny : int
-        Grid levels in the y-direction.
-
-        This means that there will be (2**Ny + 1) grid points in the y-direction.
-
-    Returns
-    -------
-    u_field: Float[Array, "N1 N2 2"]
-        Vector field of shape (2**N1 + 1, 2**N2 + 1, 2) representing the low-frequency fluctuations.
-    """
---
+++ WindGenerator/source/DataGenerator.py
@@ -0,0 +1,75 @@
+from math import *
+import numpy as np
+import torch
+
+
+####################################################################
+#   One Point Spectra Data Generator
+#   (iso, shear: Kaimal, Simiu-Scanlan, Simiu-Yeo)
+####################################################################
+
+class OnePointSpectraDataGenerator:
+
+    def __init__(self, **kwargs):
+        self.DataPoints = kwargs.get('DataPoints', None)
+        self.data_type  = kwargs.get('data_type', 'Kaimal') # 'Kaimal', 'Custom', 'Simiu-Scanlan', 'Simiu-Yeo'
+
+        self.zref= kwargs.get('zref',1)
+        self.Uref = kwargs.get('Uref',1)
+
+        if self.data_type == 'VK':
+            self.eval = self.eval_VK
+        elif self.data_type == 'Kaimal':
+            self.eval = self.eval_Kaimal
+        elif self.data_type == 'IEC':
+            self.eval = self.eval_IEC
+        elif self.data_type == 'Custom':
+            if kwargs.get('spectra_file') is not None:
+                print('Reading file' + kwargs.get('spectra_file') + '\n')
+                spectra_file=kwargs.get('spectra_file')
+                self.CustomData=np.genfromtxt(spectra_file,skip_header=1,delimiter=',')
+            else:
+                raise Exception('Custom spectra_file not found')
+        else:
+            raise Exception('No data type was provided')
+
+        if self.DataPoints is not None:
+            self.generate_Data(self.DataPoints)
+
+    def generate_Data(self, DataPoints):
+        DataValues = np.zeros([len(DataPoints), 3, 3])
+        if self.data_type == 'Custom':
+            DataValues[:,0,0]=self.CustomData[:,1]
+            DataValues[:,1,1]=self.CustomData[:,2]
+            DataValues[:,2,2]=self.CustomData[:,3]
+            DataValues[:,0,2]=-self.CustomData[:,4]
+        else:
+            for i, Point in enumerate(DataPoints):
+                DataValues[i] = self.eval(*Point)
+
+        self.Data = ( DataPoints, DataValues )
+        return self.Data
+
+    def eval_VK(self, k1, z=1):
+        C = 3.2
+        L = 0.59
+        F = np.zeros([3,3])
+        F[0,0] = 9/55  * C / (L**(-2) + k1**2) **(5/6)
+        F[1,1] = 3/110 * C * (3*L**(-2) + 8*k1**2) / (L**(-2) + k1**2) **(11/6)
+        F[2,2] = 3/110 * C * (3*L**(-2) + 8*k1**2) / (L**(-2) + k1**2) **(11/6)
+        return k1*F
+
+    def eval_Kaimal(self,k1,z=1):
+        z=self.zref
+        n = 1/(2*pi) * k1 * z
+        F = np.zeros([3,3])
+        F[0,0] = 102 * n / (1 + 33*n)**(5/3)
+        F[1,1] = 17  * n / (1 + 9.5*n)**(5/3)
+        F[2,2] = 2.1 * n / (1 + 5.3*n**(5/3))
+        F[0,2] = -12  * n / (1 + 9.6*n)**(7./3.)
+        return F
+
+    def eval_IEC(self, k1, z=1):
+        F = np.zeros([3,3])
+        return F
+
